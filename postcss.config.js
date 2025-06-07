4ed827abe177d8430718bec245253843ed70d7ec
ParthivmMS <parthivmssince2005@gmail.com>
2025-06-07 21:31:45 +0530

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,20 @@
+// File: tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
+
+// File: postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+};
